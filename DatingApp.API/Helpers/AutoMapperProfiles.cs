d8912fd5e3512b452c88328db30d58fd313c5d6d
Afam <afamezechukwu@gmail.com>
2018-10-24 16:33:44 +0100

    Extended the API by adding more details to User

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(destination => destination.PhotoUrl, option => {
+                    option.MapFrom(source => source.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(destination => destination.Age, option => {
+                    option.ResolveUsing(source => source.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(destination => destination.PhotoUrl, option => {
+                    option.MapFrom(source => source.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(destination => destination.Age, option => {
+                    option.ResolveUsing(source => source.DateOfBirth.CalculateAge());
+                });
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
